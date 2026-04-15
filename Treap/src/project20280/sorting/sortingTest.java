60646e65fef1064ee6ae51c022de767afc967cb2
Thehan <hikkaduwage.arunadewa@ucdconnect.ie>
2026-04-15 23:50:23 +0100

    junit for sortin

---
+++ Treap/src/project20280/sorting/sortingTest.java
@@ -0,0 +1,65 @@
+package project20280.sorting;
+
+import org.junit.jupiter.api.Test;
+
+public class sortingTest {
+    private static final int[] SIZES = {100, 500, 1000, 2000, 5000, 10000};
+    private static final String[] PATTERNS = {"random", "nearly_sorted", "reverse"};
+    private static final int ITERATIONS = 5;
+
+    private void printHeader(String pattern) {
+        System.out.println("\n Pattern: " + pattern);
+        System.out.println("+--------+---------------+---------------+---------------+---------------+---------------+");
+        System.out.println("|   n    |  TreapSort(ms)|   PQSort(ms)  |  TimSort(ms)  | QuickSort(ms) | MergeSort(ms) |");
+        System.out.println("+--------+---------------+---------------+---------------+---------------+---------------+");
+    }
+
+    private void printFooter() {
+        System.out.println("+--------+---------------+---------------+---------------+---------------+---------------+");
+    }
+    @Test
+    void testSortingRandom() {
+        printHeader("random");
+        for (int n : SIZES) {
+            int[] data = Benchmark.generateData(n, "random");
+            printRow(n, data);
+        }
+        printFooter();
+    }
+
+    @Test
+    void testSortingNearlySorted() {
+        printHeader("nearly_sorted");
+        for (int n : SIZES) {
+            int[] data = Benchmark.generateData(n, "nearly_sorted");
+            printRow(n, data);
+        }
+        printFooter();
+    }
+
+    @Test
+    void testSortingReverse() {
+        printHeader("reverse");
+        for (int n : SIZES) {
+            int[] data = Benchmark.generateData(n, "reverse");
+            printRow(n, data);
+        }
+        printFooter();
+    }
+
+    private void printRow(int n, int[] data) {
+        long treap = Benchmark.benchmarkTreapSort(data, ITERATIONS);
+        long pq    = Benchmark.benchmarkPQSort(data, ITERATIONS);
+        long tim   = Benchmark.benchmarkCollectionsSort(data, ITERATIONS);
+        long quick = Benchmark.benchmarkQuickSort(data, ITERATIONS);
+        long merge = Benchmark.benchmarkMergeSort(data, ITERATIONS);
+
+        System.out.printf("| %-6d | %-13.4f | %-13.4f | %-13.4f | %-13.4f | %-13.4f |%n",
+                n,
+                treap / 1_000_000.0,
+                pq    / 1_000_000.0,
+                tim   / 1_000_000.0,
+                quick / 1_000_000.0,
+                merge / 1_000_000.0);
+    }
+}
