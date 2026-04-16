93c9596bac6c237600af0812f478166e7faa0d87
suyuhhh <157200501+suyuhhh@users.noreply.github.com>
2026-04-16 14:23:21 +0100

    Update PQSort.java

--- Treap/src/project20280/sorting/PQSort.java
+++ Treap/src/project20280/sorting/PQSort.java
@@ -6,12 +6,12 @@ public class PQSort {
 
     public static void sort(int[] arr) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int val : arr) {
+        //Loop through numbers in unsorted array one by one
+        for (int val : arr) { //Put the number into the priority queue
             pq.add(val);
         }
-
-        for (int i = 0; i < arr.length; i++) {
+        //Loop through the slots of the original array from start to finish
+        for (int i = 0; i < arr.length; i++) { //Put the smallest number out if the queue
             arr[i] = pq.poll();
         }
     }
